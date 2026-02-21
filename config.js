406a5b7e01ffd2fc8370f54c9e47427419d05074
Mirel1708 <constantin.alexandru1708@gmail.com>
2026-02-21 17:21:13 +0200

    Initial upload - SnowPS website files
    SnowPS website

---
+++ config.js
@@ -0,0 +1,20 @@
+/* 
+ -- READ THIS --
+
+- You can change the server feature in index.html file and find the feature part / feature area
+- You can change your server about in index.html file and find the about part / about us area
+
+ -- CHANGE THE WEBSITE OPTION IN BELOW --
+*/
+let option = {
+    server_name: "SnowPS",
+    server_description: "SnowPS, the best growtopia private server with the best eco and best comunity",
+    owner: [
+        "Owner Server #Mirel",
+        "Owner Server#Obby The Main Owner"
+    ],
+    discord_invite_link: "https://discord.gg/snowps",
+    ip_address: "15.235.166.218",
+    host_for_android: "https://dtpshost.com/raw/SnowPS-GH"
+}
+
